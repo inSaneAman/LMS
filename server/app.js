3ac881ed036d0b13b5ddca15b0b8b795e2e8e08c
inSaneAman <162028654+inSaneAman@users.noreply.github.com>
2024-11-25 19:17:37 +0530

    Started building Course Section logics and respective controllers

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ import morgan from "morgan";
 import userRoutes from "./routes/user.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import bodyParser from "body-parser";
-
+import courseRoutes from "./routes/course.routes.js";
 config();
 
 const app = express();
@@ -17,21 +17,23 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    credentials: true,
-  })
+    cors({
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+    })
 );
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use("/ping", (req, res) => {
-  res.send("Pong");
+    res.send("Pong");
 });
 
 //routes of 3 modules
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/courses", courseRoutes);
+
 app.all("*", (req, res) => {
-  res.status(404).send("OOPS! 404 page not found");
+    res.status(404).send("OOPS! 404 page not found");
 });
 
 app.use(errorMiddleware);
